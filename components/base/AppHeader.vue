8e7f746e7a1db936cf206a964ac3907e73718bdb
shrinathprabhu <shrinathprabhu.123@gmail.com>
2023-08-14 12:47:02 +0530

    Try wrapping header and aside in div

--- components/base/AppHeader.vue
+++ components/base/AppHeader.vue
@@ -1,3 +1,5 @@
 <template>
-  <header class="h-12 border-b border-b-solid border-b-stone-200"></header>
+  <div>
+    <header class="h-12 border-b border-b-solid border-b-stone-200"></header>
+  </div>
 </template>
--- components/base/AppSidebar.vue
+++ components/base/AppSidebar.vue
@@ -1,5 +1,7 @@
 <template>
-  <aside
-    class="max-md:hidden h-full w-96 border-r border-r-solid border-r-stone-200"
-  ></aside>
+  <div>
+    <aside
+      class="max-md:hidden h-full w-96 border-r border-r-solid border-r-stone-200"
+    ></aside>
+  </div>
 </template>
